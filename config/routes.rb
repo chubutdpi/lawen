cd9e59aee4b4ddcd272e40b17c8417025a00a762
Silvio Geoffroy <sageoffroy@gmail.com>
2021-09-29 11:10:38 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'welcome#index'
+  get 'welcome/index'
+  resources :events
+  resources :localities
+  resources :departments
+  resources :provinces
+  resources :nationalities
+  resources :countries
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
